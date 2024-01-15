d3472a2331a6ba7ef57c5953670abae3181fdc18
JAC-SERVERS\reza.shalchian <reza.shalchian@a.com>
2024-01-15 12:56:17 -0500

    first session - basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,45 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import ContactList from "./components/ContactList";
 
 function App() {
-  const [count, setCount] = useState(0);
+  let message = "Hello world to FSD09 !!!";
+  const number: number = 19;
+  const values = [60, 61, 62];
+  const isDay: boolean = true;
+  // is the comment :
+  // we cannot show the object in the jsx
+  const Me = {
+    id: 1,
+    name: "reza",
+    job: "teacher",
+  };
+
+  if (Math.random() > 0.5) {
+    message = "you are lucky";
+  }
+
+  // be carefull. you can have only one Div for adjacent elements
+  //in the JSX
 
   return (
-    <div>
-      <h1>Hello world !!!</h1>
-    </div>
+    <>
+      <div>
+        <h1>{message}</h1>
+        <h1>{number}</h1>
+        <h1>{values}</h1>
+        <h1>{isDay}</h1>
+        {/* <h1>{Me}</h1> */}
+        <h1>JSON.stringify(Me)</h1>
+        <h1>{JSON.stringify(Me)}</h1>
+        <h1>{new Date().toLocaleDateString()}</h1>
+      </div>
+      <div>
+        <ContactList />
+      </div>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,17 @@ import App from "./App.tsx";
 import "./index.css";
 // import App2 from "./App2.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    {/* this is the starting point */}
-    <App />
-    {/* <App2></App2> */}
-  </React.StrictMode>
-);
+//Gets a reference to the div in in the index.html
+const el = document.getElementById("root");
+
+//Tell react to take control of that elemet.
+const root = ReactDOM.createRoot(el!);
+
+root.render(<App />);
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     {/* this is the starting point */}
+//     <App />
+//     {/* <App2></App2> */}
+//   </React.StrictMode>
+// );
